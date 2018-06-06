3a38d8e17099dc8ff086f69e92a4fb0315d93931
Ayabonga <nompetsheniayabonga@gmail>
2018-06-06 15:55:28 +0200

    made my btn return the list

--- registration_number.js
+++ registration_number.js
@@ -4,26 +4,40 @@ var RegNameDisplayElement = document.querySelector(".RegNameDisplay");
 var Type_of_TownElem = document.querySelector(".Type_of_Town");
 
 
-var NameStorage = localStorage.getItem('Names') ? JSON.parse(localStorage.getItem('Names')) : {};
+var RegistratinNumber = localStorage.getItem('Regs') ? JSON.parse(localStorage.getItem('Regs')) : {};
 
-function Registration() {
+var upperCase = document.getElementsByTagName('Input')[0]
+upperCase.oninput = function() {
+  upperCase.value = upperCase.value.toUpperCase();
+}
+document.body.onload = Registration;
 
-  var person = InpuRegElement.value
-  RegNameDisplayElement.innerHTML = person;
-  var TextNode = document.createTextNode(person);
-  var list = document.createElement("LI");
-  list.appendChild(TextNode);
-  document.getElementById("regN");
-  localStorage.setItem("Names", JSON.stringify(person));
+function Registration () {
 
-  if (person === "") {
+  var currentDiv = document.getElementById("myUL");
+  // create a new div element
+ var newDiv = document.createElement("div");
+   // and give it some content
+var newContent = document.createTextNode(InpuRegElement.value);
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+  //add the newly created element and its content into the DOM
+  document.body.appendChild(newDiv);
+
+  if (newContent === "") {
 
     return RegNameDisplayElement.innerHTML = "Please enter a Registration Number";
-
   }
+
 }
 
 AddbtnElement.addEventListener('click', function() {
-  Registration();
+  Registration(newContent);
+
+
+
+  var newContent = InpuRegElement.value
+  RegNameDisplayElement.innerHTML = newContent;
+  localStorage['Regs'] = JSON.stringify(newContent);
 
 });
