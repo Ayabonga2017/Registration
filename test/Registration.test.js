5f0c2b209661af4b80dc8efa97495ea6c1ba1018
Ayabonga <nompetsheniayabonga@gmail>
2018-06-07 16:55:19 +0200

    added some tests

--- test/Registration.test.js
+++ test/Registration.test.js
@@ -27,7 +27,7 @@ describe("Registration numbers from different Towns", function() {
 
         // returns everything for CY...
         RegNumber.addRegNumber("CY");
-        assert.deepEqual(RegNumber.myMap(), {"CY 124": 0, "CY 234": 0 });
+        assert.equal(RegNumber.myMap(), {"CY 124": 0, "CY 234": 0 });
 
   })
 
